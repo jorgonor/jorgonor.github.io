e240c070e71c71c867f250dcbd4d427747d75865
jorgonor <jorgonor88@gmail.com>
2020-08-05 16:20:42 +0200

    legacy built website migration

---
+++ README.md
@@ -0,0 +1,1 @@
+This page is built using Jekyll 2!!!
---
+++ app/app.js
@@ -0,0 +1,39 @@
+var App = (function() {
+    var App = {
+        init: function() {
+            var el = document.getElementById('cookies_notice');
+
+            if (this.getCookie('_CK')) {
+                el.style.display = 'none';
+            } else {
+                document.getElementById('cookies_notice_close').onclick = function() {
+                    el.style.display = 'none';
+                    App.setCookie('_CK', '1');
+                    return false;
+                };
+            }
+        },
+        setCookie: function(k, v) {
+           var expire = new Date();
+           expire = new Date(expire.getTime() + 7776000000);
+           document.cookie = k + "=" + v + ";expires=" + expire + ";path=/";
+        },
+        getCookie: function(k) {
+            var parts, part;
+            parts = document.cookie.split(";");
+            for (var i = parts.length - 1; i >= 0; i--) {
+                part = parts[i].replace(/^\s+/g, '').replace(/\s+$/, '');
+                if (part.indexOf(k) == 0) {
+                    parts = part.split("=");
+                    return parts[1];
+                }
+            }
+
+            return null;
+        }
+    };
+
+    App.init();
+
+    return App;
+})();
